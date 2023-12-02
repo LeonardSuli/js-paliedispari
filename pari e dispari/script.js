8a1df82d7b0ebfe65bb93cf9b7165308114680fe
LeonardSuli <leonardsuli1991@gmail.com>
2023-12-02 21:31:02 +0100

    add function pari o dispari and declare who win

--- pari e dispari/script.js
+++ pari e dispari/script.js
@@ -7,13 +7,12 @@
 
 
 //l'utente sceglie pari o dispari
-const pari_o_dispari = prompt('Scegliere tra pari o dispari');
-console.log(pari_o_dispari);
+const pari_dispari = prompt('Scegliere tra Pari o Dispari');
+console.log(pari_dispari);
 
 
 //l'utente sceglie un numero da 1 a 5
-const num = parseInt(prompt('Scegli un numero da 1 a 5'));
-
+const num = Number(prompt('Scegli un numero da 1 a 5'));
 console.log('Il numero dell\'utente è:', num);
 
 
@@ -31,16 +30,43 @@ function num_random_pc(min, max) {
 
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 
-  }
+}
   
   const num_pc = num_random_pc(1, 5);
-  
   console.log('Il numero del pc è:', num_pc);
 
 
-  //sommiamo il nemero dato dall'utente e quello del pc
+  //sommiamo il numero dato dall'utente e quello del pc
   const somma = num + num_pc
+  console.log('la somma è:' ,somma);
 
-  console.log('La somma è', somma);
 
+ // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+ function is_even(somma){
+
+    if (somma % 2 === 0) {
+
+      return 'pari';
+
+    } else {
+
+      return 'dispari';
+
+    }
+  }
   
+  // Stabiliamo se la somma è pari o dispari utilizzando la funzione
+  const risultato_pari_dispari = is_even(somma);
+  console.log('La somma è', risultato_pari_dispari);
+
+
+  // Dichiariamo chi ha vinto
+  if(pari_dispari == risultato_pari_dispari){
+
+    console.log('Complimenti hai vinto');
+
+  } else {
+
+    console.log('Mi dispiace hai perso');
+
+  }
